27e3ef6ab9cb111de923c9d0a58b4115d3228553
Cem Kesici <cemkesici51@gmail.com>
2023-11-20 17:13:40 +0300

    Initial commit

---
+++ app/src/main/java/com/cem/widgetmanager/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.cem.widgetmanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cem.widgetmanager.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding :ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
